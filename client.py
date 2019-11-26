73896692a2fd0fd4d1b0832cb69b5c7156865011
Matthew (Matthew) Wilbern <wilm3780@36drdoom.cs.uleth.ca>
2019-11-26 16:23:51 -0700

    aaaaaaaaa

--- client.py
+++ client.py
@@ -1,24 +1,20 @@
 import socket
+
 #placeholder  from https://pythontic.com/modules/socket/udp-client-server-example
- def client (msgFromClient, bytesToSend, serverAddressPort, bufferSize)
+def client (msgFromClient, serverAddressPort, bufferSize):
     #msgFromClient       = "Hello UDP Server"
-    #bytesToSend         = str.encode(msgFromClient)
     #serverAddressPort   = ("127.0.0.1", 20001)
     #bufferSize          = 1024
 
+    bytesToSend         = str.encode(msgFromClient)
     # Create a UDP socket at client side
-
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    while True:
+        # Send to server using created UDP socket
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        msg = "Message from Server {}".format(msgFromServer[0])
+        print(msg)
 
 
-
-    # Send to server using created UDP socket
-
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-
-
-    msg = "Message from Server {}".format(msgFromServer[0])
-
-    print(msg)
+client("ACK", ('', 69420), 1024)
