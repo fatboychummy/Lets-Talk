fc83bdcf59af05e7722e45266b3177c86c77cd4d
Amanda (Amanda) Munden <muna3780@36rogue.cs.uleth.ca>
2019-11-29 17:40:37 -0700

    fixing spacing issues in client

--- client.py
+++ client.py
@@ -2,22 +2,22 @@ import socket,sys
 
 #placeholder  from https://pythontic.com/modules/socket/udp-client-server-example
 
-    localPort = 55000
-    destIP = "142.66.140.186"
+localPort = 55000
+destIP = "142.66.140.186"
 
-    if len(sys.argv) != 2:
-        print("Usage: {} destination_IP_addr".format(sys.argv[0]))
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: {} destination_IP_addr".format(sys.argv[0]))
+    sys.exit(1)
 
-    try:
-        UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except:
-        print("Cannot open socket")
-        sys.exit(1)
+try:
+    UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except:
+    print("Cannot open socket")
+    sys.exit(1)
 
-    try:
-        UDPClientSocket.bind(('', 55000))
-        UDPClientSocket.sendto(b'Hello World', (sys.argv[1], localPort))
-    except OSError as err:
-        print('Cannot send: {}'.format(err.strerror))
-        sys.exit(1)
+try:
+    UDPClientSocket.bind(('', 55000))
+    UDPClientSocket.sendto(b'Hello World', (sys.argv[1], localPort))
+except OSError as err:
+    print('Cannot send: {}'.format(err.strerror))
+    sys.exit(1)
