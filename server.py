8cb93e3f38a450001303292d915e7082e809717f
Justin (Justin) Onoferychuk <onoj3620@36xavier.cs.uleth.ca>
2019-11-29 17:30:59 -0700

    server can receive stuff

--- server.py
+++ server.py
@@ -2,32 +2,32 @@ import socket,sys,errno
 import protocols
 # example from https://pythontic.com/modules/socket/udp-client-server-example
 
-    localPort   = 55000
-    bufferSize  = 1024
-    msgFromServer       = "Hello UDP Client"
-    bytesToSend         = str.encode(msgFromServer)
-    # Create a datagram socket
+localPort   = 55000
+bufferSize  = 1024
+msgFromServer       = "Hello UDP Client"
+bytesToSend         = str.encode(msgFromServer)
+# Create a datagram socket
+try:
+    UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except:
+    print("Cannot open socket")
+    sys.exit(1)
+# Bind to address and ip
+try:
+    UDPServerSocket.bind(('', localPort))
+except:
+    print("Cannot bind socket to port")
+    sys.exit(1)
+
+print("UDP server up and listening")
+# Listen for incoming datagrams
+while(True):
     try:
-        UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except:
-        print("Cannot open socket")
-        sys.exit(1)
-    # Bind to address and ip
-    try:
-        UDPServerSocket.bind(('', localPort))
-    except:
-        print("Cannot bind socket to port")
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    except OSError as err:
+        print("Cannot receive from socket: {}".format(err.strerror))
         sys.exit(1)
 
-    print("UDP server up and listening")
-    # Listen for incoming datagrams
-    while(True):
-        try:
-            bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-        except OSError as err:
-            print("Cannot receive from socket: {}".format(err.strerror))
-            sys.exit(1)
-
-        message = bytesAddressPair
-        clientMsg = "Message from Client:{}".format(message)
-        print(clientMsg)
+    message = bytesAddressPair
+    clientMsg = "Message from Client:{}".format(message)
+    print(clientMsg)
