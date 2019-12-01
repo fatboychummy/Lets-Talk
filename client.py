92c92132c021892c2f79eb99daea325f437cc25c
Matthew (Matthew) Wilbern <wilm3780@36alfred.cs.uleth.ca>
2019-12-01 16:50:21 -0700

    a

--- client.py
+++ client.py
@@ -12,19 +12,6 @@ if len(sys.argv) != 2:
     print("Usage: {} destination_IP_addr".format(sys.argv[0]))
     sys.exit(1)
 
-try:
-    UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except:
-    print("Cannot open socket")
-    sys.exit(1)
+a = protocols(sys.argv[1], localPort, 1024)
 
-try:
-    UDPClientSocket.bind(('', 55000))
-    sends = protocols.cutData("Some data yes hello b b b b hi", 4)
-    i = 0
-    for pack in sends:
-        UDPClientSocket.sendto(packet(i, 0, packet.SYN, pack).dump(), (sys.argv[1], localPort))
-    UDPClientSocket.sendto(packet(i, 0, packet.FIN, "").dump(), (sys.argv[1], localPort))
-except OSError as err:
-    print('Cannot send: {}'.format(err.strerror))
-    sys.exit(1)
+a.slidingWindow("This is a string of stuff that needs to be sent", 4, 4)
