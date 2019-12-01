9c826601ddacdcd95bd3ef528c36caad47b70a9b
Matthew (Matthew) Wilbern <wilm3780@36alfred.cs.uleth.ca>
2019-12-01 16:51:12 -0700

    b

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -44,51 +44,64 @@ class protocols:
     def slidingWindow(self, send, windowSize, maxFrames):
         temp = protocols.cutData(send, windowSize)   # cut the data
         windows = protocols.toWindowFrames(packet.SYN, *temp) # convert into frames
-        lastACK = -1   # last acked frame, set to -1 since frame 0 is not acked
-        current = 0    # current frame sending
-        # define two threads to run simultaneusly
+        self.lastACK = -1   # last acked frame, set to -1 since frame 0 is not acked
+        self.current = 0    # current frame sending
+        # define two_threads to run simultaneusly
 
         haltEvent = Event()
 
-        # Thread 1
-        def thread_1(self, current, windows, lastACK):
-            while current < len(windows) and current != lastACK:
+        #_thread 1
+        def thread_1(self, windows):
+            while self.current < len(windows) or self.current != self.lastACK:
                 # while we still have frames to send
                 # and until the final ack is recieved
                 startTime = time.time() # get the time at which we started sending
-                cAck = lastACK          # set a temporary ack check var
-                while current < lastACK + maxFrames:
+                cAck = self.lastACK          # set a temporary ack check var
+                while self.current < self.lastACK + maxFrames:
+                    print("C" + str(self.current))
+                    print("L" + str(self.lastACK))
+                    print("M" + str(maxFrames))
                     # send the next maxFrames frames
-                    currentWindow = windows[current]
+                    currentWindow = windows[self.current]
                     print("Send " + repr(currentWindow))
                     self.sock.sendto(currentWindow.dump(), (self.UDP_IP, self.UDP_PORT))
-                    current = current + 1
-                while time.time() < startTime + 0.5 and cAck == lastACK:
+                    self.current = self.current + 1
+                while time.time() < startTime + 0.5 and cAck == self.lastACK:
                     # wait x seconds (timeout) or wait until lastACK is updated
                     time.sleep(0.001)
                 if time.time() > startTime + 0.5:
-                    current = lastACK + 1
-                lastACK = lastACK + 1
+                    self.current = self.lastACK + 1
+
+            print("Done at")
+            print(self.current)
+            print(self.lastACK)
+            print(maxFrames)
             haltEvent.set()
             print("THREAD 1 DONE")
 
         def thread_2(self, halt):
             while not halt.is_set():
                 try:
-                    data = self.sock.recvfrom(1024) # buffer size can change
+                    bAPair = self.sock.recvfrom(self.bufferSize) # recieve from client
+                    binFlag = bytearray(bAPair[0][:3])  # binary flags sent in packet
+                    ackn = binFlag[1]
+                    self.current = ackn
                 except:
                     print("cannot receive")
                     sys.exit(1)
                 print("recv " + data)
             print("THREAD 2 DONE")
-            # Thread 2
+            #_thread 2
             # listen for acks
             # if the ack recieved is greater than 1 above the lastACK
             # set current back to lastACK
             # else set lastACK to the ack recieved
 
-        thread.start_new_thread(thread_1, (self, current, windows, lastACK))
-        thread.start_new_thread(thread_2, (self, haltEvent))
+        _thread.start_new_thread(thread_1, (self, windows))
+        _thread.start_new_thread(thread_2, (self, haltEvent))
+        while not haltEvent.is_set():
+            print("CHECKY " + str(self.current))
+            time.sleep(0.1)
 
 
     def slidingListen(self):
@@ -98,13 +111,12 @@ class protocols:
         data = bytearray()
         while 1:
             bAPair = self.sock.recvfrom(self.bufferSize) # recieve from client
-            data += bAPair[0][3:]   # data from client
             binFlag = bytearray(bAPair[0][:3])  # binary flags sent in packet
             raddr = bAPair[1][0]                # address to respond to
             rport = bAPair[1][1]                # port to respond to
-
+            breakflag = False
             if binFlag[2] == packet.FIN:
-                break   # if FINALIZE, stop
+                breakflag = True   # if FINALIZE, stop
 
             if binFlag[0] > lastRec + 1:
                 binFlag[0] = lastRec
@@ -113,11 +125,24 @@ class protocols:
             else:
                 # otherwise ack this packet
                 lastRec = binFlag[0]
+                data += bAPair[0][3:]               # data from client
 
             # send ACK
             self.sock.sendto(str(packet(0, binFlag[0], packet.ACK, "ack")).encode(), (raddr, rport))
-        return data
+            if breakflag:
+                break
+        return protocols.scrub(data)
     # --------------------------------------------------------------------------
+
+
+    # ----
+    # scrubs empty characters from a bytearray
+    # dat: the data to be scrubbed
+    # ----
+    @staticmethod
+    def scrub(dat):
+        dat2 = dat.split(b'\x00')
+        return dat2[0]
 
     # ----
     # data: data to be cut into smaller size and inserted into a list
