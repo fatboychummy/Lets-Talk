3e80ea7bf13599ee2950352c2ca39449108c2ffc
yugoo <uosuj461@mtroyal.ca>
2019-12-04 18:17:15 -0700

    still have to work on some stuff

--- proxy.py
+++ proxy.py
@@ -1,46 +1,61 @@
 import socket,sys
-import protocols
+from protocols import protocols
 import threading
 
 class Proxy:
     """ Used to proxy single udp connection"""
-    BUFFER_SIZE = 1024
+    localPoirt = 55002
 
-    def __init__(self, listening_addr, forward_addr):
+    def __init__(self, UDP_IP1, UDP_IP2, UDP_PORT_1, UDP_PORT_2, BUFFER_SIZE):
         print ("Server started on", listening_addr)
-        self.bind = listening_addr
-        self.target = forward_addr
+        self.UDP_IP1 = UDP_IP1
+        self.UDP_IP2 = UDP_IP2
+        self.UDP_PORT_1 = UDP_PORT_1
+        self.UDP_PORT_2 = UDP_PORT_2
+        self.BUFFER_SIZE = BUFFER_SIZE
 
         def proxy_server(self):
-            # listen for incoming connections:
+            # listen for incoming connections on port 1:
             try:
-                target = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             except:
                 print("cannot open socket")
-
-            try:
-                target.connect(self.target)
-            except OSError as err:
-                print("cannot connect to: {}".format(err.strerror))
                 sys.exit(1)
 
             try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                self.sock1.bind('', UDP_PORT_1)
             except:
-                print("cannot bind socket to port")
+                print ("couldn't bind socket 1 to port 1 recieve" )
+                sys.exit(1)
+            #thread_1 send to client
+            while True:
+                data = self.sock1.recvfrom(self.BUFFER_SIZE)
+                self.sock1.sendto(data[0], (self.UDP_IP2, self.UDP_PORT_2))
+
+
+        def proxy_client(self):
+            #listen for incoming connectons on port 2
             try:
-                s.bind(self.bind)
-
+                sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             except:
-                print ("couldn't bind port" )
-                raise SystemExit
+                print("cannot open socket")
+                sys.exit(1)
 
-            while 1:
-                dgram = s.recv(self.BUFFER_SIZE)
-                if not dgram:
-                    break
-                length = len(dgram)
-                sent = target.send(dgram)
-                if length != sent:
-                    print ("cannot send data")
-            s.close()
+            try:
+                self.sock2.bind('', UDP_PORT_2)
+            except:
+                print("couldn't bind socket 2 to port 2")
+                sys.exit(1)
+
+            #thread 2 send to server
+            while True:
+                data = self.sock2.recvfrom(self.BUFFER_SIZE)
+                sef.sock2.sendto(data[0], (self.UDP_IP1, self.UDP_PORT_1))
+
+        t1 = threading.Thread(name="Server", target=proxy_server, args=(self))
+        t2 = threading.Thread(name="Client", target=proxy_client, args=(self))
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
